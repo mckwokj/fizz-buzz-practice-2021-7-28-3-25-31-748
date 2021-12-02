53bfdf75500173a5c40e9f95e727645cea7ba9ac
Johnson Kwok <kwokjo2@oocl.com>
2021-12-02 14:10:40 +0800

    [FizzBuzz] [Johnson Kwok] feat: complete and pass test case 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,10 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public String countOff(int order) {
-        if (order % 3 == 0) {
+        if (order % 3 == 0 && order % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else if (order % 3 == 0) {
             return "Fizz";
         } else if (order % 5 == 0) {
             return "Buzz";
