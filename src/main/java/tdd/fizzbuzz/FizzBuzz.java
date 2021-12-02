8af144638418b8ba6d97ae53131d13c220532843
Johnson Kwok <kwokjo2@oocl.com>
2021-12-02 14:08:37 +0800

    [FizzBuzz] [Johnson Kwok] feat: complete and pass test case 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,8 @@ public class FizzBuzz {
     public String countOff(int order) {
         if (order % 3 == 0) {
             return "Fizz";
+        } else if (order % 5 == 0) {
+            return "Buzz";
         }
 
         return String.valueOf(order);
